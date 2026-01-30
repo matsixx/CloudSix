c8ce59fb65f0c00113326e571d8fd5b9ff2b96c6
matsix <lilmatsix@gmail.com>
2026-01-29 23:13:43 -0500

    v1.0.3 - err version number fix

--- Plugin.cs
+++ Plugin.cs
@@ -4,7 +4,7 @@ using CloudSix.Patches;
 
 namespace CloudSix
 {
-    [BepInPlugin("com.matsix.cloudsix", "CloudSix", "1.0.4")]
+    [BepInPlugin("com.matsix.cloudsix", "CloudSix", "1.0.3")]
     public class Plugin : BaseUnityPlugin
     {
         public static ManualLogSource MyLog;
