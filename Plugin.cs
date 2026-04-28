61ed458225b96cabb2296ea67050457966570d52
matsix <lilmatsix@gmail.com>
2026-04-28 18:11:25 -0400

    v3.0.3

--- Plugin.cs
+++ Plugin.cs
@@ -5,7 +5,7 @@ using CloudSix.Source;
 
 namespace CloudSix
 {
-    [BepInPlugin("com.matsix.cloudsix", "CloudSix", "3.0.2")]
+    [BepInPlugin("com.matsix.cloudsix", "CloudSix", "3.0.3")]
     public class Plugin : BaseUnityPlugin
     {
         public static ManualLogSource MyLog;
